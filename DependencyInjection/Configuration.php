b404be4f30196e29ed104dddfdfe52895c85c874
Charles Sanquer <charles.sanquer@gmail.com>
2014-08-03 16:40:38 +0200

    change service names

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('c_sanquer_twig_extra');
+        $rootNode = $treeBuilder->root('csanquer_twig_extra');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
